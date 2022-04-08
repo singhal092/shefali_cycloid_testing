35cb619308f89d133ac8910541ba50653680ef28
singhal092 <35054679+singhal092@users.noreply.github.com>
2022-04-08 08:15:36 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,28 +1,27 @@
 provider "google" {
+version = "3.11.0"
 credentials = "${file("shefali.json")}"
 project = "mohit-2022"
 region = "us-central1"
 zone = "us-central1-a"
 }
 
+resource "google_compute_instance" "instance" {
+  name         = "testshefali"
+  machine_type = "f1-micro"
+  zone         = "us-central1-a"
+  desired_status = "RUNNING"
 
 
-resource "google_compute_instance" "default" {
-name = "terraform-test"
-machine_type = "e2-medium"
+  boot_disk {
+    initialize_params {
+      image = "centos-cloud/centos-7"
+    }
+  }
 
-boot_disk {
-initialize_params {
-image = "ubuntu-1604-xenial-v20201014"
-}
-}
-
-
-
-network_interface {
-
-network = "default"
-access_config {
-}
-}
+  network_interface {
+    network = "default"
+    access_config {
+	}
+  }
 }
