c9e45e488bf063553a8ece1ca9b1f61ded70adbd
singhal092 <35054679+singhal092@users.noreply.github.com>
2022-04-13 12:51:40 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "gcs" {
-         credentials = "mohit-2022-e4a8778fcfae.json"
+         credentials = "${file("shefali.json")}"
          bucket      = "shefalitestbucket"
          prefix      = "VmOnOff"
     }
